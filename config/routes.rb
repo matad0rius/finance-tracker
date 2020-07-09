deac9555a8e93b34ff50c0d9d11c11783f4cb0a9
unknown <matad00rius@gmail.com>
2020-07-10 00:52:22 +0200

    display stock price in browser

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
   get 'my_portofolio', to: 'users#my_portofolio'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'search_stock', to: 'stocks#search'
 end
