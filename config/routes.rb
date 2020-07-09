83600f321f6eb03ac94eb6b9612370377c5b9bd8
unknown <matad00rius@gmail.com>
2020-07-09 23:56:51 +0200

    build my portofolio view and stock lookup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
+  get 'my_portofolio', to: 'users#my_portofolio'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
