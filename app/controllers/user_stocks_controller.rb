467540b3dba5d730037cb3dc980e536a8b98d9c5
unknown <matad00rius@gmail.com>
2020-07-11 15:46:10 +0200

    add remove tracking

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -11,4 +11,12 @@ class UserStocksController < ApplicationController
     redirect_to my_portofolio_path
   end
 
+  def destroy
+    sotck = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    user_stocl.destroy
+    flash[:notice] = "#{stock.ticker} was successfully removed from portofolio"
+    redirect_to my_portofolio_path
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,23 @@ class User < ApplicationRecord
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # check if ticker_symbol exist in the db 
+  def stock_already_tracked?(ticker_symbol)
+    stock = Stock.check_db(ticker_symbol)
+    return false unless stock
+    # check if the user is tracking the stock
+    user.stocks.where(id: stock.id).exists?
+  end
+  
+  # limit the number of stocks an user can track
+  def under_stock_limit?
+    user.stocks.count < 10
+  end
+
+  # check if the user can track the stock 
+  def can_track_stock?(ticker_symbol)
+    under_stock_limit? && !stock_already_tracked?(ticker_symbol)
+  end
+
 end
