5af6eeb799ea325bc72c0e57e2f94bcec3d5bfd3
unknown <matad00rius@gmail.com>
2020-07-12 10:13:35 +0200

    add frienship model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,9 @@ class UsersController < ApplicationController
   def my_portofolio
     @tracked_stocks = current_user.stocks
   end
+
+  def my_friends
+    @friends = current_user.friends
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my_portofolio', to: 'users#my_portofolio'
   get 'search_stock', to: 'stocks#search'
+  get 'my_friends', to: 'users#my_friends'
 end
